bbd459004d31608031af47f52dfbdf899cafc7f4
NATHALIA VENTURA KAWAKAMI <23111822@CFP127ALUNO.LOCAL>
2024-02-01 09:52:46 -0300

    estrutura bonitinha

---
+++ controller/controller_filme.js
@@ -0,0 +1,39 @@
+/*********************************************************************************************************
+ * Objetivo: Arquivo responsável pela validação, consistência de dados das requisições da API de Filme.
+ * Data: 1/2/24
+ * Autor: Nathalia Kawakami
+ * Versão: 1.0
+ ********************************************************************************************************/
+
+//Função para validar e inserir um novo filme.
+const setInserirNovoFilme = async() => {
+
+}
+
+//Função para validar e atualizar um filme.
+const setAtualizarFilme = async() => {
+
+}
+
+//função para excluir um filme.
+const setExcluirFilme = async() => {
+
+}
+
+//função para retornar todos os filmes.
+const getListarFilmes = async() => {
+
+}
+
+//função para buscar um filmes pelo Id.
+const getBuscarFilme = async() => {
+
+}
+
+module.exports={
+    setInserirNovoFilme,
+    setAtualizarFilme,
+    setExcluirFilme,
+    getListarFilmes,
+    getBuscarFilme
+}
---
+++ model/DAO/filme.js
@@ -0,0 +1,39 @@
+/***************************************************************************************************************************************************************************************
+ * Objetivo: Arquivo responsável pelo acesso ao banco de dados SQL, aqui faremos o CRUD na tabela de filme.
+ * Data: 1/2/24
+ * Autor: Nathalia Kawakami
+ * Versão: 1.0
+ **************************************************************************************************************************************************************************************/
+
+//Função para inserir um filme no banco de dados.
+const insertFilme = async() => {
+
+}
+
+//Função para atualizar um filme no BD.
+const updateFilme = async() => {
+
+}
+
+//função para excluir um filme no BD.
+const deleteFilme = async() =>{
+
+}
+
+//Função para listar todos os filmes do BD.
+const selectAllFilmes = async() => {
+
+}
+
+//Função para buscar um filme no Bd pelo Id.
+const selectByIdFilme = async() => {
+
+}
+
+module.exports ={
+    insertFilme,
+    updateFilme,
+    deleteFilme,
+    selectAllFilmes,
+    selectByIdFilme
+}
